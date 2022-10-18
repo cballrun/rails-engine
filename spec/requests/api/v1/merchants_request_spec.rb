4e925be25ed9ea6cc8bd2672a37b6b650dcd0863
Carter Ball <carter.k.ball@gmail.com>
2022-10-18 18:51:42 -0400

    Basic tests for Merchants index passing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,17 @@ describe "Merchants API" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merch|
+      expect(merch).to have_key(:id)
+      expect(merch[:id]).to be_a(Integer)
+
+      expect(merch).to have_key(:name)
+      expect(merch[:name]).to be_a(String)
+    end
   end
 end
