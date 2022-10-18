7b6f9951adecefa302e35d2131aa4000a059c706
Carter Ball <carter.k.ball@gmail.com>
2022-10-18 19:27:13 -0400

    All merchant test updates, beginning of merchant by id tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,16 +8,31 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchant_data = JSON.parse(response.body, symbolize_names: true)
+    
+    merchants = merchant_data[:data]
     
     expect(merchants.count).to eq(3)
 
     merchants.each do |merch|
-      expect(merch).to have_key(:id)
-      expect(merch[:id]).to be_a(Integer)
 
-      expect(merch).to have_key(:name)
-      expect(merch[:name]).to be_a(String)
+      expect(merch).to have_key(:id)
+      expect(merch[:id]).to be_a(String)
+
+      expect(merch).to have_key(:type)
+      expect(merch[:type]).to eq("merchant")
+
+      expect(merch).to have_key(:attributes)
+      expect(merch[:attributes]).to be_a(Hash)
+
+      expect(merch[:attributes]).to have_key(:name)
+      expect(merch[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
   end
 end
