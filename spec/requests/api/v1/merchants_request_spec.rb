53d89a675a418f1946f7972a9283007bebeff98f
Carter Ball <carter.k.ball@gmail.com>
2022-10-18 18:45:17 -0400

    Response working

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it 'sends a list of merchants' do
-    x = create_list(:merchant, 3)
-    binding.pry
+    create_list(:merchant, 3)
+    
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
   end
 end
